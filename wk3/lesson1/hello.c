d0786853fb0787f6f20eb6e4947f09b02b64b450
yangguang <yangguang8112@mails.jlu.edu.cn>
2023-03-22 23:52:54 +0800

    pid tid htop sys_call

--- wk3/lesson1/hello.c
+++ wk3/lesson1/hello.c
@@ -1,4 +1,4 @@
-#include "thread.h"
+#include "mythread.h"
 
 void Thello(int id) {
   while (1) {
---
+++ wk3/lesson1/mythread.h
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdatomic.h>
+#include <assert.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+
+#define NTHREAD 64
+enum { T_FREE = 0, T_LIVE, T_DEAD, };
+struct thread {
+  int id, status;
+  pthread_t thread;
+  void (*entry)(int);
+};
+
+struct thread tpool[NTHREAD], *tptr = tpool;
+
+void *wrapper(void *arg) {
+  struct thread *thread = (struct thread *)arg;
+  printf("Thread %d's TID is %lu\n", thread->id, syscall(SYS_gettid));
+  thread->entry(thread->id);
+  return NULL;
+}
+
+void create(void *fn) {
+  assert(tptr - tpool < NTHREAD);
+  *tptr = (struct thread) {
+    .id = tptr - tpool + 1,
+    .status = T_LIVE,
+    .entry = fn,
+  };
+  pthread_create(&(tptr->thread), NULL, wrapper, tptr);
+  // printf("Thread %d created with TID %lu %lu %d %d\n", tptr->id, tptr->thread, syscall(SYS_gettid), getpid(), ppid);
+  ++tptr;
+}
+
+void join() {
+  for (int i = 0; i < NTHREAD; i++) {
+    struct thread *t = &tpool[i];
+    if (t->status == T_LIVE) {
+      pthread_join(t->thread, NULL);
+      t->status = T_DEAD;
+    }
+  }
+}
+
+__attribute__((destructor)) void cleanup() {
+  join();
+}
