adc6ca8e1fa3ddf041716ede3c409a21596c3dc8
yangguang <yangguang8112@mails.jlu.edu.cn>
2023-03-22 22:39:08 +0800

    add wk3

---
+++ wk3/lesson1/hello.c
@@ -0,0 +1,15 @@
+#include "thread.h"
+
+void Thello(int id) {
+  while (1) {
+    // printf("%c", "_ABCDEFGHIJKLMNOPQRSTUVWXYZ"[id]);
+  }
+}
+
+int main() {
+  for (int i = 0; i < 6; i++) {
+    create(Thello);
+  }
+  pid_t pid = getpid();
+  printf("Main Process ID is: %d\n", pid);
+}
